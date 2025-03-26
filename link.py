4c31d99213f3711167e52163208e75c1f78c201a
Santiago Verdu <s@qmake.org>
2025-03-25 19:05:37 -0600

    ok pls

--- link.py
+++ link.py
@@ -30,7 +30,7 @@ def setup_logging(args):
 def main():
     args = parse_options()
     setup_logging(args)
-    blacklist = ["p.enc", ".git", "link.py", "README.txt", ".gitignore", "launch.sh", ".ctags"]
+    blacklist = ["p.enc", ".git", "link.py", "README.txt", ".gitignore", "launch.sh", ".ctags", ".config"]
     for x in [ x for x in os.listdir(".") if x not in blacklist ]:
         if args.remove:
             try:
